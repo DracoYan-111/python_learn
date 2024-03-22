8ccc20cabfaf8bfa6e4b4f77fe64d0cf8fc58f8c
Msif9 <2365040257@qq.com>
2024-03-22 11:40:18 +0800

    Update all

--- DracoLearnNote.py
+++ DracoLearnNote.py
@@ -49,7 +49,7 @@ number2=range(5,10,2)   #起始值，终止值，布长
 for number in number2:      #迭代范围里的数
   print(number)
 #直接调用range
-for number in range(5)
+for number in range(5):
   print(number)
 
 
@@ -57,3 +57,90 @@ for number in range(5)
   #计数，索引
 numbers=(1,2,3,3,3)
 numbers.count(3)
+#conditional statement
+def main():
+   x,y=8,4
+   if(x<y):
+      set='x is less than y'
+   else:
+      set='x is greater than y'
+print(set)
+# Python program to demonstrate    #隐式
+# type Casting 
+
+# int variable
+a = 5
+
+# typecast to float
+n = float(a)
+
+print(n)
+print(type(n))
+# Python program to demonstrate 
+# type Casting 
+
+# int variable
+a = 5
+
+# typecast to int
+n = int(a)
+
+print(n)
+print(type(n))
+# Python program to demonstrate 
+# type Casting 
+
+# int variable
+a = 5
+
+# typecast to str
+n = str(a)
+
+print(n)
+print(type(n))
+#string variable   字符串变量
+a='5'
+b='t'
+#typecast to int 类型转换为int
+n=int(a)
+print(n)
+print(type(n))
+
+print(str(b))
+print(type(b))
+#真假值
+# number=0
+# if number:
+#    print(number)
+#空列表 []
+# 空元组 （）
+# 字典 {}
+# 空集 set（）
+# 空字符串 “ ”
+# 空范围范围（0）
+#内置bool（）函数
+# define a function for checking 
+# number is even or odd 
+def even_odd(number): 
+	
+ if number % 2: 
+	
+	# since num % 2 is equal to 1     奇数
+	# which is Truthy Value 
+  return 'odd number'
+	
+ else: 
+	
+	# since num%2 is equal to 0      偶数
+	# which is Falsy Value. 
+ 	return 'even number'
+
+result1 = even_odd(7) 
+
+# prints odd 
+print(result1) 
+
+result2 = even_odd(4) 
+
+# prints even 
+print(result2) 
