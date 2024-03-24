846e1dce79aee40d5f07c3887251b05b846fd080
Msif9 <2365040257@qq.com>
2024-03-24 17:27:01 +0800

    Update Python_list.py

--- Python_list.py
+++ Python_list.py
@@ -91,6 +91,42 @@ print('Dictionary',d)
 del d[10]
 print('dictionary',d)
 
+#正则表达式
+normal='Hello\nWorld'
+print(normal)
+raw=r'Hello\nWWorld'        #原始字符串运算符“r”，转义字符的效果不会按照其含义进行转换
+print(raw)
+
+import re
+str='an example word:cat!!'
+match=re.search(r'world:\w\w\w',str)
+if match:
+    print('found',match.group())
+else:
+    print('did not find')
+#你怎么称呼三只眼睛的猪
+  ## Search for pattern 'iii' in string 'piiig'.
+  ## All of the pattern must match, but it may appear anywhere.
+  ## On success, match.group() is matched text.
+match = re.search(r'iii', 'piiig') # found, match.group() == "iii"
+match = re.search(r'igs', 'piiig') # not found, match == None
+
+  ## . = any char but \n
+match = re.search(r'..g', 'piiig') # found, match.group() == "iig"
+
+  ## \d = digit char, \w = word char
+match = re.search(r'\d\d\d', 'p123g') # found, match.group() == "123"
+match = re.search(r'\w\w\w', '@@abcd!!') # found, match.group() == "abc"
+
+#电子邮件示例
+str = 'purple alice-b@google.com monkey dishwasher'
+match = re.search(r'\w+@\w+', str)
+if match:
+    print(match.group())  ## 'b@google'
+#方括号
+match = re.search(r'[\w.-]+@[\w.-]+', str)
+if match:
+    print(match.group())  ## 'alice-b@google.com'
 
 
 
